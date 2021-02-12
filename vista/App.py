181987e8b87fc7fc0d31ef773d7261bdfd885fde
KrLits <carlosjpepe120@gmail.com>
2021-02-12 11:36:47 -0500

    Registro de usuario completo php to py

--- vista/App.py
+++ vista/App.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_mysqldb import MySQL
 from flask_assets  import Environment, Bundle
-from bcrypt
+import  bcrypt
+import datetime
+from datetime import date,datetime
 
 app = Flask(__name__)
 
@@ -57,36 +59,62 @@ def Rregistrousuario():
 
 @app.route('/registroCode', methods=['POST'])
 def Mregistrousuario():
+
+# Hace falta recuperar, guardar y devolver las imagenes https://www.tutorialspoint.com/file-upload-example-in-python
+# https://codigosimportantes.blogspot.com/2012/09/almacenar-y-recuperar-fotos-desde-mysql.html
+
     if request.method == 'POST':
-        nombre = request.form['txtNombre']
-        apellido = request.form['txtApellido']
-        tipodocumento = request.form['txtTipoDocumento']
-        numerodocumento = request.form['txtDocumento']
-        foto = request.form['txtFoto']
-        fechanacimiento = request.form['txtFechaNacimiento']
-        sexo = request.form['txtGenero']
-        rh = request.form['txtRh']
-        email = request.form['txtEmail']
-        celular = request.form['txtCelular']
-        direccion = request.form['txtDireccion']
-        estadocivil = request.form['txtEstadoCivil']
-        eps = request.form['txtEps']
-        tipoasistente = request.form['tipoAsistente']
-        fechabautizo = request.form['txtFechaBautizo']
-        nombrepastor = request.form['txtNombrePastor']
-        llenosanto = request.form['llenoSanto']
-        fechasanto = request.form['txtFechaSanto']
-        servidorlocal = request.form['servidorLocal']
-        comite = request.form['txtComite']
-        cargo = request.form['txtCargo']
+        nombre = "'" + request.form['txtNombre'] + "'"
+        apellido = "'" + request.form['txtApellido'] + "'"
+        tipodocumento = "'" + request.form['txtTipoDocumento'] + "'"
+        numerodocumento = "'" + request.form['txtDocumento'] + "'"
+        passtempencode = request.form['txtDocumento'].encode("utf-8")
+        passtempencripted = bcrypt.hashpw(passtempencode, semilla)
+        password = "'" + str(passtempencripted.decode('utf-8')) + "'"
+
+        foto =  "'" + request.form['txtFoto'] + "'"
+        print(foto)
+
+        fechanacimiento = "'" + request.form['txtFechaNacimiento'] + "'"
+        fechatemp = ((request.form['txtFechaNacimiento']))
+        date_time_obj = datetime.strptime(fechatemp, '%Y-%m-%d')
+        edadentero =  int(((datetime.now().date() - date_time_obj.date()).days/365))
+        edad = "'" + str(edadentero) + "'"
+
+
+
+        sexo = "'" + request.form['txtGenero'] + "'"
+        rh = "'" + request.form['txtRh'] + "'"
+        email = "'" + request.form['txtEmail'] + "'"
+        celular = "'" + request.form['txtCelular'] + "'"
+        direccion = "'" + request.form['txtDireccion'] + "'"
+        estadocivil = "'" + request.form['txtEstadoCivil'] + "'"
+        eps = "'" + request.form['txtEps'] + "'"
+        tipoasistente = "'" + request.form['tipoAsistente'] + "'"
+        fechabautizo = "'" + request.form['txtFechaBautizo'] + "'"
+        nombrepastor = "'" + request.form['txtNombrePastor'] + "'"
+        llenosanto = "'" + request.form['llenoSanto'] + "'"
+        fechasanto = "'" + request.form['txtFechaSanto'] + "'"
+        servidorlocal = "'" + request.form['servidorLocal'] + "'"
+        comite = "'" + request.form['txtComite'] + "'"
+        cargo = "'" + request.form['txtCargo'] + "'"
+
+        privilegio =  "'" + '1' + "'"
+
+
 
 
         cur = mysql.connection.cursor()
-        cur.execute('INSERT INTO culto (fecha, horainicio, horafinal, capacidadmax, piso) VALUES (' +  fecha  + ' , ' + horainicio + ',' + horafinal  + ' ,' + capacidadmax + ',' + piso + ')')
+        cur.execute("INSERT INTO usuarios (nombre, apellido, tipoDocumento, cedula, password, imagen, edad ,fechaNacimiento, genero, rh, tipoAsistente, correo, celular, direccion, estadoCivil, epsSisben, fechaBautizmo, pastorBautizo, llenoSanto, fecha, servidorLocal, comite, cargo, privilegio) VALUES (" +
+        nombre + " , "  + apellido + ","  + tipodocumento + " , "  + numerodocumento + " , " + password + ", "+ foto + " , "+ edad + " , "  +
+        fechanacimiento + " , "  + sexo + " , "  + rh + " , "  + tipoasistente + " , "  + email + " , "  + celular + " , "  + direccion + " , "  +
+        estadocivil + " , "  + eps + " , "  + fechabautizo + " , "  + nombrepastor + " , "  + llenosanto + " , "  + fechasanto + " , "  +
+        servidorlocal + " , "  + comite + " , "  + cargo + " , "  + privilegio + ")")
         mysql.connection.commit()
-        return render_template()
+        return render_template('index.html')
 
-
+    else:
+        return render_template('index.html')
 
 
 
